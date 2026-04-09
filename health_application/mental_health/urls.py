ecc61ee0c115400335c6b1309473fbe562f193a6
Gauravgit97 <gauravjoshicontact9760@gmail.com>
2026-04-09 19:45:19 +0530

    django home page problem

--- health_application/mental_health/urls.py
+++ health_application/mental_health/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/',views.home_page),
+    # path('/',views.home_page),
     path('index/',views.index,name='index'),
     path('general/', views.general, name='general'),
     path('diabetes/', views.diabetes, name='diabetes'),
